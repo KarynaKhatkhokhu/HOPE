8c4ee2e65cc1a532ea30d6987c632fb189dcfd81
mivsher <yay>
2025-04-16 11:32:23 +0200

    imports refactor

--- app/(tabs)/stopSkill.tsx
+++ app/(tabs)/stopSkill.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ScrollView, Appearance } from 'react-native';
 import { Card, Colors } from 'react-native-ui-lib';
 import { useThemeRefresh } from '../../hooks/useThemeRefresh';
