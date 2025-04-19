996535cd01a884859ee443574e05d370ec063339
mivsher <yay>
2025-04-19 19:02:18 +0200

    chore: create Themes constant

---
+++ constants/Theme.ts
@@ -0,0 +1,18 @@
+import { Colors } from "react-native-ui-lib";
+
+export const Themes = {
+    light: {
+      screenBG: Colors.grey80,
+      textColor: Colors.black,
+      secondary: Colors.grey60,
+      accentColor: Colors.black,
+      buttonColor: Colors.grey50,
+    },
+    dark: {
+      screenBG: Colors.grey10,
+      textColor: Colors.white,
+      secondary: Colors.grey20,
+      accentColor: Colors.white,
+      buttonColor: Colors.black,
+    },
+  };
