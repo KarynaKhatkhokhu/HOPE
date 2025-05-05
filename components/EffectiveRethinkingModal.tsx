70906c2e117b8a7e11d38f900c8e6bed6247f490
mivsher <yay>
2025-05-05 14:55:22 +0200

    chore: use color theme

--- components/EffectiveRethinkingModal.tsx
+++ components/EffectiveRethinkingModal.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
-import { Modal, View, Text, StyleSheet, Pressable, Image } from 'react-native';
+import { Modal, View, Text, StyleSheet, Pressable, Image, Appearance } from 'react-native';
 import { Colors } from 'react-native-ui-lib';
 import { useTranslation } from "react-i18next";
+import { useThemeRefresh } from '@/hooks/useThemeRefresh';
+
+export const themes = {
+  light: {
+    screenBG: Colors.grey80,
+    cardBG: Colors.grey80,
+    textColor: Colors.black,
+    myButtonColor: Colors.grey50,
+    myButtonTextColor: Colors.black
+  },
+  dark: {
+    screenBG: Colors.grey10,
+    cardBG: Colors.grey10,
+    textColor: Colors.white,
+    myButtonColor: Colors.black,
+    myButtonTextColor: Colors.white
+  },
+};
+
+const systemColorScheme = Appearance.getColorScheme();
+Colors.setScheme(systemColorScheme);
+Colors.loadSchemes(themes);
 
 export default function EffectiveRethinkingModal({
   visible,
@@ -10,14 +32,16 @@ export default function EffectiveRethinkingModal({
   visible: boolean;
   onClose: () => void;
 }) {
+  useThemeRefresh();
+  Colors.loadSchemes(themes);
   const { t } = useTranslation();
 
   return (
     <Modal visible={visible} animationType="slide" transparent>
       <View style={styles.overlay}>
         <View style={styles.modalContent}>
-          <Image style={styles.illustration} source={require(t('EffectiveRethenking.imageName'))}/>
-          <Text style={styles.title}>Effective Rethinking & Paired Relaxation</Text>
+          <Image style={styles.illustration} source={require('../assets/images/PMREFFTHINKING_EN.png')}/>
+          <Text style={styles.title}>{t("effective_rthinking.title")}Effective Rethinking & Paired Relaxation</Text>
 
           <Text style={styles.text}>
             Combine calming your body with reframing your thoughts.{"\n\n"}
@@ -45,7 +69,7 @@ const styles = StyleSheet.create({
     padding: 24,
   },
   modalContent: {
-    backgroundColor: Colors.white,
+    backgroundColor: Colors.cardBG,
     borderRadius: 12,
     padding: 24,
     width: '100%',
@@ -55,11 +79,11 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 12,
-    color: Colors.black,
+    color: Colors.textColor,
   },
   text: {
     fontSize: 14,
-    color: Colors.grey30,
+    color: Colors.textColor,
     marginBottom: 16,
   },
   closeButton: {
