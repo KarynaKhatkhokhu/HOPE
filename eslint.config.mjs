a753e1301700221395e28c1ab2d9394adaf8620d
mivsher <yay>
2025-08-02 15:28:30 +0200

    chore: setup husky and lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,12 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+export default [
+  {
+    ignores: ['commitlint.config.js'],
+  },
+  ...tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended
+  )
+];
